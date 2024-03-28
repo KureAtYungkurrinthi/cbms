3c4f2ccd5bb1a80e1c96296b52e87fc160632d89
Wang Shengfan <wang2147@flinders.edu.au>
2024-03-29 01:40:02 +1030

    add error handling in controllers

--- cbms-backend/controllers/usersController.js
+++ cbms-backend/controllers/usersController.js
@@ -1,19 +1,41 @@
 const User = require('../models/User');
 
 const getAllUsers = async (req, res) => {
-    const users = await User.findAll({
-        attributes: {exclude: ['password']}
-    });
-    return users ? res.json(users) : res.status(204).json({'message': 'No users found'});
+    try {
+        const users = await User.findAll({
+            attributes: {exclude: ['password']}
+        });
+        if (users.length > 0) {
+            return res.json(users);
+        } else {
+            return res.status(204).json({'message': 'No users found'});
+        }
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        return res.status(500).json({'message': 'Internal Server Error'});
+    }
 };
 
 const getUserById = async (req, res) => {
-    if (!req?.params?.id) return res.status(400).json({'message': 'User ID required'});
-    const user = await User.findByPk(req.params.id, {
-        attributes: {exclude: ['password']}
-    });
-    return user ? res.json(user) : res.status(204).json({'message': 'User not found'});
+    try {
+        const userId = req?.params?.id;
+        if (!userId) {
+            return res.status(400).json({'message': 'User ID required'});
+        }
+        const user = await User.findByPk(userId, {
+            attributes: {exclude: ['password']}
+        });
+        if (user) {
+            return res.json(user);
+        } else {
+            return res.status(204).json({'message': 'User not found'});
+        }
+    } catch (error) {
+        console.error('Error fetching user by ID:', error);
+        return res.status(500).json({'message': 'Internal Server Error'});
+    }
 };
+
 
 const createUser = async (req, res) => {
 
