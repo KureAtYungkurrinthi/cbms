d0c1226a91fbae66735a44812927c25f1c216a09
Wang Shengfan <wang2147@flinders.edu.au>
2024-03-31 19:22:45 +1030

    cleaning up auth codes

--- cbms-backend/lib/verifier.js
+++ cbms-backend/lib/verifier.js
@@ -2,16 +2,23 @@ const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) return res.sendStatus(401);
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({message: 'Authorization header missing or malformed'});
+    }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_SECRET, (err, decoded) => {
-        if (err) return res.sendStatus(403); //invalid token
-        req.id = decoded.id;
-        req.name = decoded.name;
-        req.email = decoded.email;
-        req.role = decoded.role;
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            if (err.name === 'JsonWebTokenError') {
+                return res.status(403).json({message: 'Invalid token'});
+            } else if (err.name === 'TokenExpiredError') {
+                return res.status(403).json({message: 'Token expired'});
+            } else {
+                return res.status(500).json({message: 'Internal Server Error'});
+            }
+        }
+        req.decoded = decoded;
         next();
     });
-}
+};
 
 module.exports = {verifyJWT};
