cf188da127869a0768c9fab83d6d53fb58ebf96a
Wang Shengfan <wang2147@flinders.edu.au>
2024-05-01 16:20:37 +0930

    minor format meetings route

--- cbms-backend/routes/meetings.js
+++ cbms-backend/routes/meetings.js
@@ -5,14 +5,14 @@ const meetingsController = require('../controllers/meetingsController');
 const methodNotAllowed = async (req, res) => res.sendStatus(405);
 
 router.route('/')
-  .get(meetingsController.getAllMeetings)
-  .post(meetingsController.createMeeting)
-  .all(methodNotAllowed);
+    .get(meetingsController.getAllMeetings)
+    .post(meetingsController.createMeeting)
+    .all(methodNotAllowed);
 
 router.route('/:id')
-  .get(meetingsController.getMeetingById)
-  .put(meetingsController.updateMeeting)
-  .delete(meetingsController.deleteMeeting)
-  .all(methodNotAllowed);
+    .get(meetingsController.getMeetingById)
+    .put(meetingsController.updateMeeting)
+    .delete(meetingsController.deleteMeeting)
+    .all(methodNotAllowed);
 
 module.exports = router;
