c83e8119059f26873976c3ef43430049b481b43c
Wang Shengfan <wang2147@flinders.edu.au>
2024-05-03 23:33:41 +0930

    create agendas route

---
+++ cbms-backend/controllers/agendasController.js
@@ -0,0 +1,15 @@
+const {Agenda, Meeting, User} = require('../models/Agenda');
+
+const getAgendas = async (req, res) => {
+}
+
+const createAgendas = async (req, res) => {
+}
+
+const updateAgendas = async (req, res) => {
+}
+
+const deleteAgendas = async (req, res) => {
+}
+
+module.exports = {getAgendas, createAgendas, updateAgendas, deleteAgendas};
--- cbms-backend/routes/meetings.js
+++ cbms-backend/routes/meetings.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const meetingsController = require('../controllers/meetingsController');
+const agendasController = require('../controllers/agendasController');
 
 const methodNotAllowed = async (req, res) => res.sendStatus(405);
 
@@ -13,6 +14,13 @@ router.route('/:id')
     .get(meetingsController.getMeetingById)
     .put(meetingsController.updateMeeting)
     .delete(meetingsController.deleteMeeting)
+    .all(methodNotAllowed);
+
+router.route('/:id/agendas')
+    .get(agendasController.getAgendas)
+    .post(agendasController.createAgendas)
+    .put(agendasController.updateAgendas)
+    .delete(agendasController.deleteAgendas)
     .all(methodNotAllowed);
 
 module.exports = router;
