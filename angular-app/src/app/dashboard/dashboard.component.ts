b8f9817941cd5ceb0684d2c4b70f60fdf286c142
Kaung San Kyaw <kskthedeveloper@gmail.com>
2024-05-08 12:33:09 +0930

    update dashboard filering meeting

--- angular-app/src/app/dashboard/dashboard.component.ts
+++ angular-app/src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import {AddMeetingComponent} from "src/app/add-meeting/add-meeting.component";
 import {Meeting} from "src/app/_models/meeting.model";
 import {AuthenticationService} from "src/app/_services/authentication.service";
 import {MeetingListService} from "src/app/_services/meeting-list/meeting-list.service";
+import {User} from "src/app/_models/user";
 
 @Component({
   selector: 'app-dashboard',
@@ -14,15 +15,29 @@ export class DashboardComponent implements OnInit {
   // bsModalRef: BsModalRef | undefined;
 
   public meetings: Meeting[] = [];
+  user: User;
 
-  constructor(private meetingListService:MeetingListService) { }
+  constructor(private meetingListService:MeetingListService, private authenticationService: AuthenticationService) {
+    this.user = authenticationService.userValue;
+  }
 
   ngOnInit(): void {
     this.meetingListService.getMeetings().subscribe(meetings => {
       console.log("checking meetings in dashboard");
       console.log(meetings);
-      this.meetings = meetings;
+
+      if (this.user.role == 'admin') {
+        this.meetings = meetings;
+      } else {
+        this.meetings = this.filterMeetingsByAttendeeId(meetings, this.user.id);
+      }
     });
+  }
+
+  filterMeetingsByAttendeeId(meetings, attendeeId) {
+    return meetings.filter(meeting =>
+      meeting.attendees.some(attendee => attendee.id === attendeeId)
+    );
   }
 
   addMeeting() {
