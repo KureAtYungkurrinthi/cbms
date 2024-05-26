f54a036ae64aa67f3485efde5fbc366b2ecc2c4e
Kaung San Kyaw <kskthedeveloper@gmail.com>
2024-05-26 10:15:59 +0930

    minor bug fixed

--- angular-app/src/app/_helpers/jwt.interceptor.ts
+++ angular-app/src/app/_helpers/jwt.interceptor.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-import { environment } from '@environments/environment';
-import { AuthenticationService } from '@app/_services';
+import {AuthenticationService} from "src/app/_services/authentication.service";
+import { environment } from "src/environments/environment";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
